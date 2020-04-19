2778201e72959743af3a5874ae149384f91b3007
OuYangWei <oyw1006@163.com>
2020-04-19 20:02:02 +0800

    修改日志显示设置

--- src/main/java/oyw/gp/oyr/DemoApplication.java
+++ src/main/java/oyw/gp/oyr/DemoApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@MapperScan(value = "mapper") // 扫描mapper包
+@MapperScan("oyw.gp.oyr.mapper")
 public class DemoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
