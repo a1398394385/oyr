0e0a71c151b83bf593a596b3bb513f645d39863b
a1398394385 <xys159753@gmail.com>
2020-04-20 19:29:23 +0800

    新增 前端页面显示控制器

---
+++ src/main/java/oyw/gp/oyr/controller/PageController.java
@@ -0,0 +1,24 @@
+package oyw.gp.oyr.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class PageController
+{
+    @GetMapping("/home")
+    public String home() {
+        return "/home";
+    }
+
+    @GetMapping("/register")
+    public String register() {
+        return "/register";
+    }
+
+    @GetMapping("/login")
+    public String login() {
+        return "/login";
+    }
+
+}
