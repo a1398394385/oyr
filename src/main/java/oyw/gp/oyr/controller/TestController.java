5d6aa6aefb10ce9e7aec9e1239b78d2eabf9a51d
a1398394385 <xys159753@gmail.com>
2020-04-22 01:37:47 +0800

    删除不必要的测试方法

--- src/main/java/oyw/gp/oyr/controller/TestController.java
+++ src/main/java/oyw/gp/oyr/controller/TestController.java
@@ -1,7 +1,6 @@
 package oyw.gp.oyr.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -11,14 +10,4 @@ public class TestController
     public String htm() {
         return "/testAJAX";
     }
-
-    @GetMapping("/rec")
-    public String rec(){
-        return "/rec";
-    }
-    @GetMapping("/recycle")
-    public String recy(){
-        return "/recycle";
-    }
-
 }
