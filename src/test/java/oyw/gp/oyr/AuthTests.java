2308c7fdef3247245344234e2aa721dbd26a6f71
a1398394385 <xys159753@gmail.com>
2020-04-22 01:34:24 +0800

    新增 session测试

---
+++ src/test/java/oyw/gp/oyr/AuthTests.java
@@ -0,0 +1,17 @@
+package oyw.gp.oyr;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@Transactional
+public class AuthTests
+{
+
+    @Test
+    public void testAuth() {}
+}
