478642c3972945eee8dc795039a5620655308066
a1398394385 <xys159753@gmail.com>
2020-04-22 01:33:29 +0800

    引入静态资源

---
+++ src/main/resources/static/js/home.js
@@ -0,0 +1,58 @@
+let app = new Vue({
+    el: '#app',
+    data: {
+        brandId: 1,
+        phones: null,
+        auth: false,
+    },
+    beforeMount: function () {
+        axios.get("/phone/brand/1")
+            .then(res => {
+                if (res.data.status == "success") {
+                    this.phones = res.data.data
+                }
+                else {
+                    alert("数据错误，请稍后重试")
+                    location.href = "/home";
+                }
+            })
+            .catch(err => {
+                alert("您的网络异常，请刷新后重试")
+                location.href = "/home";
+                console.error(err);
+            })
+    },
+    methods: {
+        getPhonesData: function (brandId) {
+            this.brandId = brandId
+            if (brandId > 0)
+                axios.get("/phone/brand/" + brandId)
+                    .then(res => {
+                        if (res.data.status == "success") {
+                            this.phones = res.data.data
+                        }
+                        else {
+                            alert("数据错误，请稍后重试")
+                            location.href = "/home";
+                        }
+                    })
+                    .catch(err => {
+                        alert("您的网络异常，请刷新后重试")
+                        location.href = "/home";
+                        console.error(err);
+                    })
+        },
+        logout: function () {
+            axios.post("/logout")
+                .then(res => {
+                    alert(res.data.data)
+                    location.reload()
+                })
+                .catch(err => {
+                    alert("您的网络异常，请刷新后重试")
+                    location.href = "/home";
+                    console.error(err);
+                })
+        }
+    },
+});
---
+++ src/main/resources/static/js/recycle.js
@@ -0,0 +1,83 @@
+let app = new Vue({
+    el: '#app',
+    data: {
+        colorOption: null,
+        storageOption: null,
+        buyingOption: null,
+        bodyOption: null,
+        screenOption: null,
+        displayOption: null,
+        serviceOption: null,
+        IDOption: null,
+        featureOption: [],
+        model: null,
+        colors: null,
+        storages: null,
+        price: null,
+        image: null,
+        auth: false
+    },
+    beforeMount: function () {
+        let phoneId = window.location.href.split("/").pop()
+        axios.get("/phone/" + phoneId)
+            .then(res => {
+                this.model = res.data.data.model
+                this.colors = res.data.data.color.split("-")
+                this.storages = res.data.data.storage.split("-")
+                this.price = res.data.data.price
+                this.image = res.data.data.image
+            })
+            .catch(err => {
+                alert("您的网络异常，请刷新后重试")
+                location.href = "/home";
+                console.error(err);
+            })
+    },
+    methods: {
+        valuation: function () {
+            alert("您的最终估价为 " + this.finalPrice + " 元")
+
+        },
+        logout: function () {
+            axios.post("/logout")
+                .then(res => {
+                    alert(res.data.data)
+                    location.reload()
+                })
+                .catch(err => {
+                    alert("您的网络异常，请刷新后重试")
+                    location.href = "/home";
+                    console.error(err);
+                })
+        },
+    },
+    watch: {
+    },
+    computed: {
+        canValuation: function () {
+            return this.colorOption != null &&
+                this.storageOption != null &&
+                this.buyingOption != null &&
+                this.bodyOption != null &&
+                this.screenOption != null &&
+                this.displayOption != null &&
+                this.serviceOption != null &&
+                this.IDOption != null
+        },
+        discount: function () {
+            return this.storageOption +
+                this.bodyOption +
+                this.screenOption +
+                this.displayOption +
+                this.serviceOption +
+                this.featureOption.length +
+                (this.IDOption * 2);
+        },
+        finalPrice: function () {
+            if (this.discount >= 20)
+                return 0
+            else
+                return Math.floor(this.price * (20 - this.discount) * 0.05)
+        }
+    }
+});
