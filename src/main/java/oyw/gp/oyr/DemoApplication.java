382349b777575e0bafc716821dc25933eb634061
root <root@MrXYS-Computer.localdomain>
2020-04-24 03:59:42 +0800

    feat(Service): 开启 Spring Data Cache

--- src/main/java/oyw/gp/oyr/DemoApplication.java
+++ src/main/java/oyw/gp/oyr/DemoApplication.java
@@ -3,10 +3,13 @@ package oyw.gp.oyr;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
 @MapperScan("oyw.gp.oyr.mapper")
-public class DemoApplication {
+@EnableCaching
+public class DemoApplication
+{
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
