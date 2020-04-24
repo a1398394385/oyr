31b80f3ff516de294da3ab08cf06a386e8fc91bd
a1398394385 <xys159753@gmail.com>
2020-04-24 19:26:10 +0800

    refactor(Templates): 重构 登录注册 页面

---
+++ src/main/resources/static/layout/js/login.js
@@ -0,0 +1,53 @@
+let app = new Vue({
+    el: 'form',
+    data: {
+        telephone: "",
+        password: "",
+    },
+    computed: {
+        disabled: function () {
+            return !this.isCorrectTel ||
+                !this.isCorrectPass
+        },
+        isCorrectTel: function () {
+            return /^1(3|4|5|6|7|8|9)\d{9}$/.test(this.telephone)
+        },
+        isCorrectPass: function () {
+            return this.password.length >= 8
+        },
+        disabledStyle: function () {
+            if (this.disabled)
+                return 'background-color: #E6E6E6';
+        },
+        telephoneStyle: function () {
+            if (this.telephone.length != 0 && !this.isCorrectTel)
+                return 'border: 1px solid red';
+        },
+        passwordStyle: function () {
+            if (this.password.length != 0 && !this.isCorrectPass)
+                return 'border: 1px solid red';
+        }
+    }
+});
+
+
+$(function () {
+    $(":submit").click((event) => {
+        event.preventDefault();
+        $.ajax({
+            type: "post",
+            url: "/login",
+            contentType: "application/json;charset=UTF-8",
+            data: JSON.stringify($('form').serializeJSON()),
+            dataType: "json",
+            success: (response) => {
+                console.log(response);
+
+                if (response.status == "success")
+                    window.location.href = '/home';
+                if (response.status == "filed")
+                    alert(response.message);
+            }
+        });
+    })
+});
---
+++ src/main/resources/static/layout/js/register.js
@@ -0,0 +1,69 @@
+let app = new Vue({
+    el: 'form',
+    data: {
+        username: "",
+        telephone: "",
+        password: "",
+        address: "",
+    },
+    computed: {
+        disabled: function () {
+            return !this.isCorrectName ||
+                !this.isCorrectTel ||
+                !this.isCorrectPass ||
+                !this.isCorrectAddress
+        },
+        isCorrectName: function () {
+            return this.username != ""
+        },
+        isCorrectTel: function () {
+            return /^1(3|4|5|6|7|8|9)\d{9}$/.test(this.telephone)
+        },
+        isCorrectPass: function () {
+            return this.password.length >= 8
+        },
+        isCorrectAddress: function () {
+            return this.address != ""
+        },
+        disabledStyle: function () {
+            if (this.disabled)
+                return 'background-color: #E6E6E6';
+        },
+        usernameStyle: function () {
+            if (false)
+                return 'border: 1px solid red';
+        },
+        telephoneStyle: function () {
+            if (this.telephone.length != 0 && !this.isCorrectTel)
+                return 'border: 1px solid red';
+        },
+        passwordStyle: function () {
+            if (this.password.length != 0 && !this.isCorrectPass)
+                return 'border: 1px solid red';
+        },
+        addressStyle: function () {
+            if (false)
+                return 'border: 1px solid red';
+        }
+    }
+});
+
+
+$(function () {
+    $(":submit").click((event) => {
+        event.preventDefault();
+        $.ajax({
+            type: "POST",
+            url: "/user/",
+            contentType: "application/json;charset=UTF-8",
+            data: JSON.stringify($('form').serializeJSON()),
+            dataType: "json",
+            success: (response) => {
+                if (response.status == "success")
+                    window.location.href = '/login';
+                if (response.status == "filed")
+                    alert(response.message);
+            }
+        });
+    })
+});
