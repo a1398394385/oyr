cecb0a5bd050c57fd80400fcaf3c924f27366fd5
a1398394385 <xys159753@gmail.com>
2020-05-02 02:22:08 +0800

    feat(common): 新增公共方法类

---
+++ src/main/java/oyw/gp/oyr/common/Public.java
@@ -0,0 +1,15 @@
+package oyw.gp.oyr.common;
+
+import java.util.Random;
+
+public class Public
+{
+    public static String randomCode() {
+        StringBuilder str = new StringBuilder();
+        Random random = new Random();
+        for (int i = 0; i < 6; i++) {
+            str.append(random.nextInt(10));
+        }
+        return str.toString();
+    }
+}
