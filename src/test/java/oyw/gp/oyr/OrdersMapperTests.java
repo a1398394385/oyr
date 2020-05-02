f5ad0326fdab341c1655d57eb727bd2368d33433
a1398394385 <xys159753@gmail.com>
2020-05-03 01:09:01 +0800

    feat(test): 新增测试类

---
+++ src/test/java/oyw/gp/oyr/OrdersMapperTests.java
@@ -0,0 +1,33 @@
+package oyw.gp.oyr;
+
+import java.util.List;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+import lombok.extern.slf4j.Slf4j;
+import oyw.gp.oyr.entity.Orders;
+import oyw.gp.oyr.mapper.OrdersMapper;
+
+@Slf4j
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@Transactional
+public class OrdersMapperTests
+{
+    @Autowired
+    OrdersMapper ordersMapper;
+
+    @Test
+    public void testGetOrdersByUserId() {
+        List<Orders> orders = ordersMapper.getOrdersByUserId(new Long(3));
+        log.error("===========================================", "");
+        log.error(orders.get(0).toString(), "");
+        Assert.assertEquals(orders.size(), 4);
+    }
+}
